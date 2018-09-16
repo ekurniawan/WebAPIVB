f53e85dabf2326eda0fcf763bbc2ea6362416280
erikur <erick@actual-training.com>
2018-09-16 19:42:31 +0700

    menambahkan data excel

---
+++ WebClientAPI/Controllers/SaveExcelSampleController.vb
@@ -0,0 +1,79 @@
+﻿Imports System.Web.Mvc
+Imports OfficeOpenXml
+Imports OfficeOpenXml.Style
+Imports System.IO
+
+
+Namespace Controllers
+    Public Class SaveExcelSampleController
+        Inherits Controller
+
+        Private _mhsDAL As MahasiswaDAL
+
+        Public Sub New()
+            _mhsDAL = New MahasiswaDAL()
+        End Sub
+
+
+        ' GET: SaveExcelSample
+        Function Index() As ActionResult
+            Dim lstMhs = _mhsDAL.GetAll()
+
+            Dim dataExcel As New ExcelPackage()
+            'membuat worksheet
+            Dim myWorksheet = dataExcel.Workbook.Worksheets.Add("MySheet")
+            myWorksheet.DefaultRowHeight = 12
+
+            'menambahkan header judul
+            myWorksheet.Row(1).Height = 20
+            myWorksheet.Row(1).Style.HorizontalAlignment = ExcelHorizontalAlignment.Left
+            myWorksheet.Row(1).Style.Font.Bold = True
+            myWorksheet.Cells(1, 1).Value = "Daftar Mahasiswa"
+
+            'header table
+            myWorksheet.Row(2).Height = 20
+            myWorksheet.Row(2).Style.HorizontalAlignment = ExcelHorizontalAlignment.Left
+            myWorksheet.Row(2).Style.Font.Bold = True
+            myWorksheet.Cells(2, 1).Value = "Nim"
+            myWorksheet.Cells(2, 2).Value = "Nama"
+            myWorksheet.Cells(2, 3).Value = "IPK"
+
+            Dim recIndex = 3
+            For Each _student In lstMhs
+                myWorksheet.Cells(recIndex, 1).Value = _student.Nim
+                myWorksheet.Cells(recIndex, 2).Value = _student.Nama
+                myWorksheet.Cells(recIndex, 3).Value = _student.IPK
+                recIndex += 1
+            Next
+
+            'ratain kolom
+            myWorksheet.Column(1).AutoFit()
+            myWorksheet.Column(2).AutoFit()
+            myWorksheet.Column(3).AutoFit()
+
+            Dim excelName = "myExcelFileSample.xlsx"
+            Dim memStream As New MemoryStream(dataExcel.GetAsByteArray())
+            Return File(memStream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", excelName)
+
+
+            'Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            'Response.AddHeader("content-disposition", $"attachment; filename={excelName}.xlsx")
+
+            'Using memStream As New MemoryStream
+            '    Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            '    Response.AddHeader("content-disposition", $"attachment; filename={excelName}.xlsx")
+            '    dataExcel.SaveAs(memStream)
+            '    memStream.WriteTo(Response.OutputStream)
+            '    Response.Flush()
+            '    Response.End()
+
+            'End Using
+
+            'Dim memStream As New MemoryStream(dataExcel.GetAsByteArray())
+
+        End Function
+
+
+
+    End Class
+End Namespace
---
+++ WebClientAPI/DAL/MahasiswaDAL.vb
@@ -0,0 +1,124 @@
+﻿Imports System.Data.SqlClient
+Imports Dapper
+Imports System.Threading.Tasks
+
+Public Class MahasiswaDAL
+
+    Public Sub Create(obj As Mahasiswa)
+        Using conn As New SqlConnection(MyHelper.GetConnStr())
+            Dim strSql = "insert into Mahasiswa(Nim,Nama,IPK) 
+                          values(@Nim,@Nama,@IPK)"
+            Dim param = New With {.Nim = obj.Nim, .Nama = obj.Nama, .IPK = obj.IPK}
+            Try
+                conn.Execute(strSql, param)
+            Catch sqlEx As SqlException
+                Throw New Exception($"Number:{sqlEx.Number} Desc:{sqlEx.Message}")
+            Catch ex As Exception
+                Throw New Exception($"Error: {ex.Message}")
+            End Try
+        End Using
+    End Sub
+
+    Public Sub Update(obj As Mahasiswa)
+        Using conn As New SqlConnection(MyHelper.GetConnStr())
+            Dim strSql = "update Mahasiswa set Nama=@Nama,IPK=@IPK 
+                          where Nim=@Nim"
+            Dim param = New With {.Nama = obj.Nama, .IPK = obj.IPK, .Nim = obj.Nim}
+            Try
+                conn.Execute(strSql, param)
+            Catch sqlEx As SqlException
+                Throw New Exception($"Number:{sqlEx.Number} Desc:{sqlEx.Message}")
+            Catch ex As Exception
+                Throw New Exception($"Error: {ex.Message}")
+            End Try
+        End Using
+    End Sub
+
+    Public Sub Delete(id As String)
+        Using conn As New SqlConnection(MyHelper.GetConnStr())
+            Dim strSql = "delete from Mahasiswa where Nim=@Nim"
+            Dim param = New With {.Nim = id}
+            Try
+                conn.Execute(strSql, param)
+            Catch sqlEx As SqlException
+                Throw New Exception($"Number:{sqlEx.Number} Desc:{sqlEx.Message}")
+            Catch ex As Exception
+                Throw New Exception($"Error: {ex.Message}")
+            End Try
+        End Using
+    End Sub
+
+    Public Function GetAll() As IEnumerable(Of Mahasiswa)
+        Using conn As New SqlConnection(MyHelper.GetConnStr())
+            Dim strSql = "select * from Mahasiswa order by Nama asc"
+            Return conn.Query(Of Mahasiswa)(strSql)
+
+            'Dim lstMhs As New List(Of Mahasiswa)
+            'Dim cmd As New SqlCommand(strSql, conn)
+            'conn.Open()
+            'Dim dr As SqlDataReader
+            'dr = cmd.ExecuteReader
+            'If dr.HasRows Then
+            '    While dr.Read
+            '        lstMhs.Add(New Mahasiswa With {
+            '                   .Nim = dr("Nim").ToString(),
+            '                   .Nama = dr("Nama").ToString(),
+            '                   .IPK = CDbl(dr("IPK"))})
+            '    End While
+            'End If
+            'dr.Close()
+            'cmd.Dispose()
+            'conn.Close()
+            'Return lstMhs
+        End Using
+    End Function
+
+    Public Function GetById(id As String) As Mahasiswa
+        Using conn As New SqlConnection(MyHelper.GetConnStr())
+            Dim strSql = "select * from Mahasiswa 
+                          where Nim=@Nim"
+            Dim param = New With {.Nim = id}
+            Dim result = conn.QuerySingle(Of Mahasiswa)(strSql, param)
+            Return result
+        End Using
+    End Function
+
+    Public Function GetByName(nama As String) As IEnumerable(Of Mahasiswa)
+        Using conn As New SqlConnection(MyHelper.GetConnStr())
+            Dim strSql = "select * from Mahasiswa
+                          where Nama like @Nama"
+            Dim param = New With {.Nama = $"%{nama}%"}
+            Dim results = conn.Query(Of Mahasiswa)(strSql, param)
+
+            Return results
+        End Using
+    End Function
+
+    Public Async Function GetByNameAsync(nama As String) As Task(Of IEnumerable(Of Mahasiswa))
+        Using conn As New SqlConnection(MyHelper.GetConnStr())
+            Dim strSql = "select * from Mahasiswa
+                          where Nama like @Nama"
+            Dim param = New With {.Nama = $"%{nama}%"}
+            Dim results = Await conn.QueryAsync(Of Mahasiswa)(strSql, param)
+            Return results
+        End Using
+    End Function
+
+    Public Sub BulkInsertData(lstData As List(Of Mahasiswa))
+        Using conn As New SqlConnection(MyHelper.GetConnStr())
+            Dim dt As New DataTable
+            dt.Columns.Add("Nim")
+            dt.Columns.Add("Nama")
+            dt.Columns.Add("IPK")
+
+            For Each mhs In lstData
+                dt.Rows.Add(mhs.Nim, mhs.Nama, mhs.IPK)
+            Next
+
+            Using sqlBulk As New SqlBulkCopy(conn)
+                sqlBulk.DestinationTableName = "Mahasiswa"
+                sqlBulk.WriteToServer(dt)
+            End Using
+        End Using
+    End Sub
+End Class
