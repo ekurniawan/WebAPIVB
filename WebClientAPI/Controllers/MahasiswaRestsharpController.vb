a861fc83ce20181acac417407d5d7724253df3ae
erikur <erick@actual-training.com>
2018-09-27 13:21:46 +0700

    menambahkan restsharp services

---
+++ WebClientAPI/Controllers/MahasiswaRestsharpController.vb
@@ -0,0 +1,80 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class MahasiswaRestsharpController
+        Inherits Controller
+
+        Private _services As MahasiswaServicesRestsharp
+
+        Public Sub New()
+            _services = New MahasiswaServicesRestsharp()
+        End Sub
+
+        ' GET: MahasiswaRestsharp
+        Function Index() As ActionResult
+            Dim results = _services.GetAll()
+            Return View(results)
+        End Function
+
+        ' GET: MahasiswaRestsharp/Details/5
+        Function Details(id As String) As ActionResult
+            Dim result = _services.GetById(id)
+            'ViewBag.Nama = result.Nama
+            'ViewData("IPK") = result.IPK
+            Return View(result)
+        End Function
+
+        ' GET: MahasiswaRestsharp/Create
+        Function Create() As ActionResult
+            Return View()
+        End Function
+
+        ' POST: MahasiswaRestsharp/Create
+        <HttpPost()>
+        Function Create(mhs As Mahasiswa) As ActionResult
+            Try
+                _services.InsertData(mhs)
+                Return RedirectToAction("Index")
+            Catch ex As Exception
+                ViewBag.Error = $"Error: {ex.Message}"
+                Return View(mhs)
+            End Try
+        End Function
+
+        ' GET: MahasiswaRestsharp/Edit/5
+        Function Edit(id As String) As ActionResult
+            Dim result = _services.GetById(id)
+            Return View(result)
+        End Function
+
+        ' POST: MahasiswaRestsharp/Edit/5
+        <HttpPost()>
+        Function Edit(mhs As Mahasiswa) As ActionResult
+            Try
+                _services.UpdateData(mhs)
+                Return RedirectToAction("Index")
+            Catch ex As Exception
+                ViewBag.Error = $"Error: {ex.Message}"
+                Return View()
+            End Try
+        End Function
+
+        ' GET: MahasiswaRestsharp/Delete/5
+        Function Delete(id As String) As ActionResult
+            Dim result = _services.GetById(id)
+            Return View(result)
+        End Function
+
+        ' POST: MahasiswaRestsharp/Delete/5
+        <HttpPost()>
+        Function Delete(mhs As Mahasiswa) As ActionResult
+            Try
+                _services.DeleteData(mhs)
+                Return RedirectToAction("Index")
+            Catch ex As Exception
+                ViewBag.Error = $"Error {ex.Message}"
+                Return View()
+            End Try
+        End Function
+    End Class
+End Namespace
---
+++ WebClientAPI/Services/MahasiswaServicesRestsharp.vb
@@ -0,0 +1,67 @@
+﻿Imports System.Threading.Tasks
+Imports RestSharp
+
+Public Class MahasiswaServicesRestsharp
+    Private _client As RestClient
+
+    Public Sub New()
+        _client = New RestClient("http://localhost:52965")
+    End Sub
+
+    Public Function GetAll() As IEnumerable(Of Mahasiswa)
+        Dim request = New RestRequest("api/Mahasiswa", Method.GET) With {
+            .RequestFormat = DataFormat.Json}
+        Dim response = _client.Execute(Of List(Of Mahasiswa))(request)
+        If response.Data Is Nothing Then
+            Throw New Exception(response.ErrorMessage)
+        End If
+
+        Return response.Data
+    End Function
+
+    Public Function GetById(nim As String) As Mahasiswa
+        Dim request = New RestRequest("api/Mahasiswa/{id}", Method.GET) With {
+            .RequestFormat = DataFormat.Json}
+        request.AddParameter("id", nim, ParameterType.UrlSegment)
+        Dim response = _client.Execute(Of Mahasiswa)(request)
+        If response.Data Is Nothing Then
+            Throw New Exception(response.ErrorMessage)
+        End If
+
+        Return response.Data
+    End Function
+
+    Public Sub InsertData(mhs As Mahasiswa)
+        Dim request = New RestRequest("api/Mahasiswa", Method.POST) With {
+            .RequestFormat = DataFormat.Json}
+        request.AddBody(mhs)
+        Dim response = _client.Execute(request)
+        If response.StatusCode <> Net.HttpStatusCode.OK Then
+            Throw New Exception($"Gagal Insert Data {response.ErrorMessage} {response.StatusCode}")
+        End If
+    End Sub
+
+    Public Sub UpdateData(mhs As Mahasiswa)
+        Dim request = New RestRequest("api/Mahasiswa/{id}", Method.PUT) With {
+            .RequestFormat = DataFormat.Json}
+        request.AddParameter("id", mhs.Nim, ParameterType.UrlSegment)
+        request.AddBody(mhs)
+
+        Dim response = _client.Execute(request)
+        If response.StatusCode <> Net.HttpStatusCode.OK Then
+            Throw New Exception($"Gagal Update Data {response.ErrorMessage}")
+        End If
+    End Sub
+
+    Public Sub DeleteData(mhs As Mahasiswa)
+        Dim request = New RestRequest("api/Mahasiswa/{id}", Method.DELETE) With {
+            .RequestFormat = DataFormat.Json}
+        request.AddParameter("id", mhs.Nim, ParameterType.UrlSegment)
+
+        Dim response = _client.Execute(request)
+        If response.StatusCode <> Net.HttpStatusCode.OK Then
+            Throw New Exception($"Gagal Delete Data {response.ErrorMessage}")
+        End If
+    End Sub
+
+End Class
